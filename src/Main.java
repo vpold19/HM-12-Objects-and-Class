8e0f7465e17615b2d7ffe7abeae69d582a7d7d83
vadim <vadimpoldolin19@gmail.com>
2023-03-12 00:43:49 +0100

    3

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,25 @@
 
 public class Main {
-    public static void main(String[] args) {
+    public static  Book createBook(String bookName, int publishingYear){
         Book stalker = new Book();
-        stalker.bookName = "STALKER";
-        System.out.println("Название - " + stalker.bookName);
+        stalker.bookName=bookName;
+        stalker.publishingYear=publishingYear;
+        return stalker;
+    }
+    public static Author createAuthor(String name, String surname){
+        Author vadim = new Author();
+        vadim.authorName=name;
+        vadim.authorSurname=surname;
+        return vadim;
+    }
 
-        stalker.publishingYear = 2007;
+    public static void main(String[] args) {
+        Book stalker = createBook("STALKER", 2007);
+        System.out.println("Название - " + stalker.bookName);
         System.out.println("Год издания - " + stalker.publishingYear);
 
-        Author vadim = new Author();
-        vadim.authorName = "Vadim";
+        Author vadim = createAuthor("Vadim", "Poldolin");
         System.out.println("Имя автора - " + vadim.authorName);
-
-        vadim.authorSurname = "Poldolin";
         System.out.println("Фамилия автора - " + vadim.authorSurname);
     }
 }
