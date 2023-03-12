e2c864b90ff1bdec37120355251bedca2ff7e691
vadim <vadimpoldolin19@gmail.com>
2023-03-12 13:27:00 +0100

    Все условия выполнены

--- src/Book.java
+++ src/Book.java
@@ -3,16 +3,21 @@ import java.time.Year;
 public class Book {
    private String bookName;
    private int publishingYear;
+   private Author author;
 
-    public Book (String Bookname, int publishingYear){
-        this.bookName=Bookname;
+    public Book (String bookName, int publishingYear, Author author){
+        this.bookName=bookName;
         this.publishingYear=publishingYear;
+        this.author=author;
     }
     public String getBookName(){
         return this.bookName;
     }
     public int getPublishingYear(){
         return this.publishingYear;
+    }
+    public Author getAuthor(){
+        return this.author;
     }
     public void setPublishingYear(int publishingYear){
         this.publishingYear=publishingYear;
